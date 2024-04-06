b0c95d188a318e6e616ad8d2ec25fdcd6d449c1e
Javier Valverde <xagot.developer@gmail.com>
2024-04-06 12:45:17 -0500

    fix: Error: The `initialStatus` argument for `Prerender` must be a natural number. (attempt)

--- app/api/transactions-by-category/route.ts
+++ app/api/transactions-by-category/route.ts
@@ -7,17 +7,13 @@ export async function GET(request: NextRequest) {
   const currentDate = new Date();
   currentDate.setDate(1);
 
-  try {
-    const { searchParams } = new URL(request.url);
-    const date = searchParams.get("date");
-    const from =
-      date?.concat("T00:00:00") || currentDate.toISOString().slice(0, 10).concat("T00:00:00");
-    const to = lastDayOfMonth(from).toISOString().slice(0, 10).concat("T23:59:59");
+  const { searchParams } = new URL(request.url);
+  const date = searchParams.get("date");
+  const from =
+    date?.concat("T00:00:00") || currentDate.toISOString().slice(0, 10).concat("T00:00:00");
+  const to = lastDayOfMonth(from).toISOString().slice(0, 10).concat("T23:59:59");
 
-    const data = await fetchTransactionsByCategory(from, to);
+  const data = await fetchTransactionsByCategory(from, to);
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return Response.error();
-  }
+  return NextResponse.json(data);
 }
